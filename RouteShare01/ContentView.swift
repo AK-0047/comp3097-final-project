027d1d25216707ea0a58d2cb7618047a4456ae52
Anshul <101416629@georgebrown.ca>
2025-02-24 14:59:26 -0500

    Initial Commit

---
+++ RouteShare01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RouteShare01
+//
+//  Created by Anshul Dharmendra Kamboya on 2025-02-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RouteShare01/RouteShare01App.swift
@@ -0,0 +1,17 @@
+//
+//  RouteShare01App.swift
+//  RouteShare01
+//
+//  Created by Anshul Dharmendra Kamboya on 2025-02-24.
+//
+
+import SwiftUI
+
+@main
+struct RouteShare01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
